e1f1dd1e372eeda6fb161adc70250d285100f108
shankar-2807 <mashalkarshankar9@gmail.com>
2026-03-23 18:52:05 +0530

    Add files via upload

---
+++ Memory Allocation.py
@@ -0,0 +1,20 @@
+#integer
+i = 10
+num = 10
+print(id(i))
+print(id(num))
+
+#string 
+str1 = 'abc'
+str2 = 'abc'
+str3 = str1
+print(id(i))
+print(id(str2))
+print(id(str3))
+
+#list
+li1 = [10,20]
+li2 = [10, 20]
+print(id(li1))
+print(id(li2))
+
