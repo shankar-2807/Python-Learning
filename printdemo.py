98b04d7e652e4b7e4b9860307ec5b81f23abecd7
shankar-2807 <mashalkarshankar9@gmail.com>
2026-03-23 18:51:35 +0530

    Add files via upload

---
+++ printdemo.py
@@ -0,0 +1,28 @@
+print(10)
+print("Hello World")
+print('''Please choose option from below
+      1. Beer
+      2. Vodka
+      3. Red bull
+      4. Old Monk
+      ''')
+
+#variable printing
+var = [10, 20, 30]
+print(var)
+
+#2 parameters in print
+print("List:",var)
+
+#concatenation
+name = "Shankar Mashalkar"
+print("Name:"+name)
+
+#operation
+print(10+20)
+
+#string formatting
+print(f'Name : {name}, \n Var : {var}')
+
+print(10+'abc')
+
