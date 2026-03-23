d45f08e97dce92f3cb0aa53122424e4a6775728c
shankar-2807 <mashalkarshankar9@gmail.com>
2026-03-23 18:51:00 +0530

    Add files via upload

---
+++ Installation.py
@@ -0,0 +1,21 @@
+####Installation
+#1. Search python download on google
+#2. Visit original website
+#3. Download recommonded latest version for relevant os.
+#4. Double click on downloaded file
+#5. Installation window will appear
+#   Check Add python.exe to path
+#   & then click on install now
+#6. Then green scale window will appear.
+#7. Successfully installed window will appear.
+
+####After installation cross check
+#1. Search idle (python) on search bar of window
+#2. Open result idle.
+#3. write hello world program & run
+#4. If successfully program ran, then we can say that the installation is completed.
+
+
+#Run program
+#python folder_path/file_name.py
+
