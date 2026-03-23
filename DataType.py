c791a1cd3311b246c4eb4784e0c43a661bd87c3a
shankar-2807 <mashalkarshankar9@gmail.com>
2026-03-23 18:53:04 +0530

    Add files via upload

---
+++ DataType.py
@@ -0,0 +1,53 @@
+####Numberic
+
+#1. integer
+num = 99
+print(type(num))
+
+#2. float
+pi = 3.14
+print(type(pi))
+
+#3. complex
+com = 10 + 7j
+print(type(com))
+
+####Sequential
+#1. String
+str1 = "Firstbit solutions is an educational institute."
+print(type(str1))
+
+#2. List
+li = [10, 20, 50, 30, 40, 'abc', 3.14]
+print(type(li))
+
+#3. Tuple
+tu = (4, 6, 8, 6.7, 'abc')
+print(type(tu))
+
+#4. range
+ra = range(1, 11)
+print(type(ra))
+
+
+####Set type
+#1. set
+s1 = {1, 2, 3, 4, 'abc', 3.14}
+print(type(s1))
+#2. frozenset
+fs1 = frozenset([3, 4, 'a', 3.14, 'b'])
+print(type(fs1))
+
+####Mapping
+#1. Dictionary
+dict1 = {1:'Python', 2:'Java', 3:'C'}
+print(type(dict1))
+
+####Boolean - True/False
+val = True
+print(type(val))
+
+####Nonetype
+
+var1 = None
+print(type(var1))
