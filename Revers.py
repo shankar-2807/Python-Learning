6ce392d553581374347955b59f7146a12ba4716f
shankar-2807 <mashalkarshankar9@gmail.com>
2026-03-24 10:02:23 +0530

    Add files via upload

---
+++ Revers.py
@@ -0,0 +1,20 @@
+# Numberic datatype
+
+# String
+var1 = "Hello World!"
+var1 = 'Hello World'
+
+var1 = "student's data"
+var1 = 'My name is "Shankar Mashalkar".'
+
+var1 = '''My data "Shankar Mashalkar". Student's data.
+ line no. 2.
+line no. 3.
+line no.4.'''
+
+var1 = """My data "Shankar Mashalkar". Student's data.
+ line no. 2.
+line no. 3.
+line no.4."""
+print(var1)
+print(type(var1))
