e6e6d9c8242ea4e754c4250dc3089544ecbd4c9b
shankar-2807 <mashalkarshankar9@gmail.com>
2026-03-24 10:01:22 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,130 @@
+#1. Addition ( + )
+i = 10
+j = 20
+res = i + j
+print(res)
+str1 = 'Firstbit '
+str2 = 'Solution'
+res = str1 + str2
+
+print(res)
+
+#2. Substraction ( - )
+res = 45 -18
+print(res)
+
+#3. Multiplication ( * )
+res = 3 * 3
+print(res)
+
+#4. Division
+res = 36/3
+print(res)
+res = 5/2
+print(res)
+#5. Floor Division ( // )
+res = 5 // 2
+res = 58 // 20
+res = 5 // 10
+
+#6. Modules ( % )
+res = 5 % 2
+res = 5 % 10
+res = 19 % 9
+res = 385 % 10
+res = 385 % 20
+
+#Exponential (**)
+res = 8**4
+res = i**j
+
+print(res)
+
+#### Comparision operator
+#1. Less than (<)
+print (i < j) #10<20
+
+#2. Greater than (>)
+print (i > j) #10>20
+
+#3. Less than or Equal to (<=)
+print (i <= j) #10<20
+
+#4.Greater than or Equal to (>=)
+print (i >= j) #10>=20
+print (i >= j) #20>=10
+
+#5. Exact equal to (==)
+print( i == j )
+
+#6. Not qual to (!=)
+print(i != j)
+
+#### Logical Operator
+#1. AND
+print( True and True) #only true otherwise false
+
+#2. OR
+print(False or False) #only false otherwise true
+
+#3. not
+print( not True ) #True to False, False to True
+
+#####Assignment Operator
+#1. (=)
+var = 10
+
+#2. (+=)
+var +=10 #var = var + 10, var +=1 : var = var +1
+#3. -= 
+var -= 5
+#4. *= 
+var *= 2
+#5. /= 
+var /= 5
+#6. //= 
+var //= 3
+#7. **= 
+var **= 2
+print(var)
+
+####Membership Operator
+li = [10, 25, 30, 58]
+li2 = li
+li3 = [10, 25, 30, 58]
+#1. in
+print(i in li) #True
+print(j in li) #False
+
+#2. not in
+print(i not in li) 
+print(j not in li)
+
+#####Identity Operator
+#1.is
+print(i is li)
+print(id(li))
+print(id(li2))
+print(li3 is li) #False
+print(id(li3))
+
+#2. is not
+print(li3 is not li)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
