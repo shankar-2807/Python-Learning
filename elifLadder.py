eff0cedd04f221fd58e6ac98dcfa211ebc83bd55
shankar-2807 <mashalkarshankar9@gmail.com>
2026-03-28 19:33:08 +0530

    Add files via upload

---
+++ elifLadder.py
@@ -0,0 +1,9 @@
+num = int(input("Enter number to check:"))
+
+if(num == 0):
+    print("Number is zero.")
+elif(num > 0):
+    print("Number is positive.")
+else:
+    print("Number is negative.")
+
