0277e1f64dfa59f569f74d0c4eb69e2ba6b32d69
shankar-2807 <mashalkarshankar9@gmail.com>
2026-03-29 08:52:52 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,19 @@
+####numberic 
+#1. integer 
+num = 90
+print(type(num))
+
+#2. float
+pi = 3.14
+print(type(pi))
+
+#3. complex
+com = 10 + 7j
+print(type(com))
+
+
+a = complex(input("Enter the first complex number: "))
+b = complex(input("Enter the second complex number: "))
+
+result = a + b
+print(f"The sum is: {result}")
