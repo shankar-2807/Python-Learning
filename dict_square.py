1de7c29039cf8a8aac6a39ea3c653686b48b1e0d
shankar-2807 <mashalkarshankar9@gmail.com>
2026-03-30 11:52:05 +0530

    Add files via upload

---
+++ dict_square.py
@@ -0,0 +1,10 @@
+# Squares.
+n = int(input("Enter Number: "))
+squares = {x: x * x for x in range(1,n+1)}
+print(squares)
+
+# Qubes.
+n = int(input("Enter Number: "))
+Qubes = {x: x **3 for x in range(1,n+1)}
+print(Qubes)
+
