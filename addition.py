fee3f988999be96f41fafe94071cd7674c654cdf
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-03 14:19:36 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,16 @@
+#Example 1.
+li= [10, 20, 30, 70, 50, 130]
+sum = 0
+for i in range(0, len(li)): 
+    sum = sum + li[i]
+print(sum)
+
+
+
+#Example 2.
+li= [10, 20, 30, 40, 50, 60]
+sum = 0
+for i in range(0, len(li)): 
+    sum = sum + li[i]
+print(sum)
+
