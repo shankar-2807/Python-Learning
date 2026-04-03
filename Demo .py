76b4d0b4c5d0613ab37c8b333996152f6c104811
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-03 14:19:55 +0530

    Add files via upload

---
+++ Demo .py
@@ -0,0 +1,16 @@
+li = [10, 3.14,'python']
+#print(type(li))
+
+print(li)
+
+#Unordered Ds 
+#s = { 1, 2, 3, 3.14, 4,'a',5,6,'abcd'}
+#print(s)
+
+print(id(li))
+li[1] = 60
+print(id(li))
+print(li)
+
+
+
