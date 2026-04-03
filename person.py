0d660af550d612767ab010a9984db153f1e33d5c
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-03 14:17:33 +0530

    Add files via upload

---
+++ person.py
@@ -0,0 +1,15 @@
+# roles of variable.
+# Firstv letter should be capitl.
+
+class Person:
+
+    #behaviour / method / function
+    def setData(self):
+        print("This is set Data method.")
+
+#creating object
+obj1 = Person()
+
+#calling method of an object
+obj1.setData()
+
