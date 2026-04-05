66c1ce8ba5f7d09a1ef28c15c55d08df9fd7c16b
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-05 20:35:38 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,17 @@
+#denoted by
+tuple = (10,)
+print(tuple)
+
+#Heterogenous
+tuple = ()
+print(tuple)
+
+#Ordered / Sequence 
+tuple = (10, 20, 30, 40, 50)
+print(tuple)
+
+#Changable / Immutable 
+
+# IMP = (faster than list)
+
+
