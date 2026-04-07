cff1ff3550951952acc7d14ee58b891859cb55e2
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-07 16:12:44 +0530

    Add files via upload

---
+++ demo2.py
@@ -0,0 +1,49 @@
+str = 'FirstBit Solutions'
+
+print(str.capitalize())   # only first latter Cpaital.
+
+print(str.count("s"))     # k-sensetive.
+
+print(str.endswith("ions"))     # true.
+
+print(str.find("Bit"))         # find index.
+
+print(str.index("Bit"))       # find index.
+
+print(str.isalnum())      # if Alphabetic or Numeric then True.
+
+print(str.isalpha())      # Only Alphabet.
+
+print(str.isdigit())     # Only Digits.
+
+print(str.islower())     #
+
+print(str.isspace())     #
+
+print(str.isupper())
+
+
+
+li = ['The' , 'IT' , 'Training' , 'Company' ]
+print(' ,'.join(li))
+
+print(str.lower())       # All String in lower.
+
+print(str.replace('Bit' , 'Byte'))     # Replace bit to Byte.
+
+print(str.split(' '))     # By Default value is Space.
+
+print(str.startswith("First "))
+
+print(str.strip(" "))
+
+print(str.swapcase())
+
+print(str.title())       # sentence any word starting should be Capital.
+
+print(str.upper())       # Every letter should be Capital.
+
+
+
+
+
