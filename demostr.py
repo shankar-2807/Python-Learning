b358668ccb6fa59a2fe05000f53fa5f65cb33d26
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-07 16:12:22 +0530

    Add files via upload

---
+++ demostr.py
@@ -0,0 +1,24 @@
+# 1. Structure : '', "", ''' ''', """ """.
+str1 = 'Firstbit Solution'
+str2 = "Firstbit Solution"
+str3 = ''' This is first line.
+This is second line.'''
+str4 = """ This is first line.
+This is second line."""
+print(str2)
+print(str4)
+
+print(type(str4))
+
+
+# 2. Types of Dta : set of character - alphabetes, digits, special symbols, space
+str5 = 'FirstBit Solutions'
+
+#3. Sequence : Ordered
+
+#4. Changable: Immutable
+str5[0] = 'A'    #gives error
+
+
+
+
