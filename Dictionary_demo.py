2fb1d48cac15a96d4042b09df9eed87bf033f2c0
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-08 19:16:28 +0530

    Add files via upload

---
+++ Dictionary_demo.py
@@ -0,0 +1,13 @@
+####  DICTIONARY    ###
+
+# 1. It is denoted by = {key:value}
+
+# 2. Type = (Hetregenous)  
+
+# 3. Ordered  = (Ordered , python version 3.6)
+
+# 4. Changable = 
+# (key = Immutable  ,, key -  Unique )  
+# (value = Mutable  ,, value - can be duplicate)
+
+
