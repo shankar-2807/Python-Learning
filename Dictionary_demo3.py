91945289758bcc80c72b9c7722061095663e7045
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-09 18:54:26 +0530

    Add files via upload

---
+++ Dictionary_demo3.py
@@ -0,0 +1,17 @@
+dict = {'eid':101, 'ename':'Shankar', 'sal':45000}
+#print(dict)
+
+#Fetch value
+print(dict['ename'])
+
+#Update / Replace Value
+dict['sal'] = 100000
+#print(dict)
+
+# print(dict['name]) #Gives KeyError
+#Update item 
+dict['add'] = 'pune'
+
+print(dict)
+
+
