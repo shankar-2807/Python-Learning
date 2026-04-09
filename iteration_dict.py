8b6b3b621e9674fc84b9bf09ecf4c143df066d61
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-09 18:54:49 +0530

    Add files via upload

---
+++ iteration_dict.py
@@ -0,0 +1,18 @@
+dict = {1:"Python" , 2:"c" , 3:"R"}
+
+for ele in dict:
+    print(ele)
+
+for ele in dict.values():
+    print(ele)
+
+for key, value in dict.items():
+    print(f'{key} : {value}')
+
+
+
+
+
+
+
+    
