956c28a31350c2f2ae04f9b423726bc9e08107da
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-09 18:54:02 +0530

    Add files via upload

---
+++ Dictionary_demo1.py
@@ -0,0 +1,24 @@
+#1. Structure - {key : value}
+
+dict = {1:"Python", 2:"c", 3:"Data Sciennce"}
+print(type(dict))
+
+#2. Type of Data - Heterogenous
+
+dict = {1:"Python", "Name":"FirstBit Solution",('a','b'):['a','b']}
+print(dict)
+
+#3. Sequence - Ordered 
+
+
+#4. Changable - key: Immutable   , value - Mutable
+
+#dict = {1:"Python", ['a','b']:"list"}
+
+#5. key: Unique, value: Duplicate
+dict = {1:"python",2:"java", 2:"c", 3:"c"}      
+print(dict)
+
+
+
+#   Home Work  =  hashable &  Unhashable  (Reasearch)
