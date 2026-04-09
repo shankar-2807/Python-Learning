cd72cc63478fcebdb39d99a475b849ea1ed0c4fd
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-09 19:01:40 +0530

    Add files via upload

---
+++ methods.py
@@ -0,0 +1,24 @@
+dict = {1:"Python", 2:"c", 3:"R"}
+
+print(dict.clear())
+
+dict2 = dict.copy()
+print(dict2)
+
+print(dict.get(2))
+
+print(dict.items())
+
+print(dict.keys())
+
+dict.pop(2)  ##
+
+dict.popitem()
+
+dict.update({4:"Go", 5:"PHP"})
+
+print(dict.values())
+
+
+print(dict)
+
