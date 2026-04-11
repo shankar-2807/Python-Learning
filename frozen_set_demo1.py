89e4327daea338dc04ba7de3f8a9d481139c0e62
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-11 19:24:45 +0530

    Add files via upload

---
+++ frozen_set_demo1.py
@@ -0,0 +1,15 @@
+#1. Structure: set(), {10,20,30}
+s1 = frozenset({10, 20, 30, 40})
+
+print(type(s1))
+
+#2. type of Data : (Heterogenous)
+s2 = frozenset({'a', 10, 20, 3.14})
+print(s2)
+
+#3. Sequence   :  (Unordered)
+print(s2)
+
+# Changable : (Immutable)
+
+
