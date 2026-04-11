0c06a53e5fb268a2926092b4281569626bc4d24e
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-11 19:24:08 +0530

    Add files via upload

---
+++ frozen_set_demo.py
@@ -0,0 +1,10 @@
+### FROZEN SET ###
+
+#1. It is denoted by ({10, 20, 30, 40}) 
+
+#2. Type of Data  (Heterogenous)  ,   Unique data(not changable)
+
+#3. Sequance    (Ordered)
+
+#4. Changable   (Mutable , Not editable)
+
