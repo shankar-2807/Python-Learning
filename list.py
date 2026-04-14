81c322def060eff977bdfa51bc0171565875bdae
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-14 18:03:08 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,33 @@
+li = [43, 21, 65, 50, 21 ,21 ,21]
+
+#li.append(18)
+#li.append([20, 30])
+
+#li.clear()
+
+#li2=li.copy()
+#li2 = li
+
+#print(li.count(20))
+
+#li.extend([10, 20, 30])
+
+#print(li.index(65))
+#print(li.index(65))
+#print(li.index(21))
+
+#li.insert(1,7)
+
+#li.pop()
+#li.pop(10)
+
+#li.remove(65)
+
+#li.reverse()
+#li.sort()  #ascending order   #doesn't work on nested list
+#li.sort(reverse=True)  #descending order
+print(li)
+
+
+
+
