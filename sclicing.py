41397413f8924daaf99706104dd68883846d4392
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-15 19:09:01 +0530

    Add files via upload

---
+++ sclicing.py
@@ -0,0 +1,22 @@
+li = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+print(li[0:2])
+print(li[2:6:2])
+print(li[1:5:3])
+print(li[0:9:2])
+print(li[2:4])
+print(li[4:8:6])
+print(li[0:100])
+print(li[5:8:6])
+print(li[6:9:6])
+
+print(li[:5])
+print(li[0:])
+print(li[:])
+print(li[::])
+print(li[::-1])
+print(li[7:2:-2])
+
+
+li.append(110)
+print(li)
+
