26e3a84fa96272ca08cbf7a92180c312cbab8356
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-17 13:50:32 +0530

    Add files via upload

---
+++ default_parameter2.py
@@ -0,0 +1,10 @@
+def emp(eid, ename, esal, edept):
+    print("EID:", eid)
+    print("ENAME:", ename)
+    print("SALARY:", esal)
+    print("DEPT:", edept)
+
+emp(ename = "Shankar", eid = 101, esal= 400000, edept = "Trainer")
+
+
+
