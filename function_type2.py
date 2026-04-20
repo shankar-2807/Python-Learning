741fe1bf1fde4315e505c4d4575d5fbda26c0878
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-20 18:44:50 +0530

    Add files via upload

---
+++ function_type2.py
@@ -0,0 +1,13 @@
+#with passing parameters
+#without returning value
+
+#defintion of function 
+def subtraction(num1, num2):
+    print("subtraction:", num1 - num2)
+
+x = int(input("Enter a number 1:"))
+y = int(input("Enter a number 2:"))
+
+#calling of function
+subtraction(x,y)
+
