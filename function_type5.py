d1205b5b47a43054be088729d8feb8e31b5a3477
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-21 18:25:08 +0530

    Add files via upload

---
+++ function_type5.py
@@ -0,0 +1,12 @@
+#with passing parameter
+#with returing value/result
+
+def rem(dividend,  divisor):
+    return dividend % divisor
+
+x = int(input("Enter dividend:"))
+y = int(input("Enetr divisor:"))
+
+res= rem(x,y)
+print("Remainder: ", res)
+
