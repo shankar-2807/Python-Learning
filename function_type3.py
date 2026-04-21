4b007bca6de2283bd82bfc8ee34ed1f00f49f899
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-21 18:24:00 +0530

    Add files via upload

---
+++ function_type3.py
@@ -0,0 +1,13 @@
+def multiplication(num1, num2):
+    print("multiplication:", num1 * num2 )
+
+x = int(input("Enter a number 1:"))
+y = int(input("Enter a number 2:"))
+
+multiplication(x, y)   #Positional argument
+
+
+
+
+
+
