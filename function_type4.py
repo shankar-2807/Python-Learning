9d82d9d176039c93aaedfe0ab72884e88cc91ed0
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-21 18:24:32 +0530

    Add files via upload

---
+++ function_type4.py
@@ -0,0 +1,12 @@
+#without passing parameters
+#with returing value
+
+def division():
+    num1 = 45
+    num2 = 10
+    res = num1 / num2
+    return res
+
+output = division()
+print("Display result:",output)
+
