3a9b1c448b875bf20926326e4f8abb0ac042f764
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-22 19:52:22 +0530

    Add files via upload

---
+++ Forlooping.py
@@ -0,0 +1,22 @@
+Name = 'Shankar'
+for i in Name:
+ print(i)
+
+
+person = {'name': 'Shankar', 'age': 22}
+for key, value in person.items():
+    print(key, value)
+
+
+colors = [ "Red" , "Yellow" , "Green" , "Orange"]
+for color in colors:
+ print(color, end= ',')
+
+
+for i in range(5):
+    print(i)
+
+for i in range(11, 20, 2):  # start=11, stop=20 (exclusive), step=2
+    print(i)
+
+
