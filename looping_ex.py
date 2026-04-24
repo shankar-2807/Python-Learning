a4c603736c9b80571a76244b05be82b0f5fd2e7f
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-24 17:27:12 +0530

    Add files via upload

---
+++ looping_ex.py
@@ -0,0 +1,15 @@
+#WAP to reverse a string.
+string = input("Enter a string: ")
+rev_str = ''
+for char in string:
+    rev_str = char + rev_str
+    print(rev_str) 
+
+
+
+string = input("Enter a string: ")
+rev_str = ''
+for char in string:
+    rev_str = char + rev_str
+print(rev_str) 
+
