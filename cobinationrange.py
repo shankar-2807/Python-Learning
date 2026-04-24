a9ea44c448ce13fefc4493de3d1e821c311fe980
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-24 17:24:18 +0530

    Add files via upload

---
+++ cobinationrange.py
@@ -0,0 +1,10 @@
+#WAP to print Even No between 1 to 100
+for i in range(1,100):
+     if( i %2 == 0):
+        print(i)
+
+#WAP to print Odd No between 1 to 100
+for i in range(1,100):
+     if( i %2 != 0):
+        print(i)
+      
