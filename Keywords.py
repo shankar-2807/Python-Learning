4aa1df2c861ddcc2ae6dd21efa62936d764bb64d
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-25 18:15:15 +0530

    Add files via upload

---
+++ Keywords.py
@@ -0,0 +1,24 @@
+#Pass
+#If you want to neglect block indentation error.
+for i in range(1, 10):
+    pass
+
+    break
+#If you want to stop the loop execution.
+for i in range(1, 10):
+    if(i == 5):
+        break
+    #Print(i)
+
+    continue
+#If you want to step current iteration.
+for i in range(1, 10):
+    if(i == 5):
+        print(i)
+else:
+    print()  
+#It will execute if loop execution successfully completed.
+for i in range(1,10):
+    pass
+else: pass
+
