7b32b6e5bd56ad5c381ce75f0c8009f0b48a9dcd
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-26 18:26:34 +0530

    Add files via upload

---
+++ multiTable.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number: "))
+n = 1
+while(n <= 10):
+    print(num * n)
+    n = n + 1
+
+num = int(input("Enter a number: "))
+n = 1
+while(n <= 10):
+    print(f'2 * {num} = {num * n}')
+    n += 1
