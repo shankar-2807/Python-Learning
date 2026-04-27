f2a56c6fe05a1994db6f82b64c3f7d0820680980
shankar-2807 <mashalkarshankar9@gmail.com>
2026-04-27 18:22:45 +0530

    Add files via upload

---
+++ primeno.py
@@ -0,0 +1,9 @@
+#WAP TO CHECK GIVEN NUMBER PRIME OR NOT.
+
+num = int(input("Enter a number:"))
+for i in range(2, num //2 + 1):
+    if(num % i==0):
+        print("Not a prime.")
+else:
+    print("Prime.")
+
