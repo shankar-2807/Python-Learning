a75269fb3f0e3e05951bca96c07588efb5370a8d
shankar-2807 <mashalkarshankar9@gmail.com>
2026-05-03 19:07:18 +0530

    Add files via upload

---
+++ pattern4.py
@@ -0,0 +1,8 @@
+row = int(input("Enter value for row: "))
+column = int(input("Enetr value for column: "))
+for i in range(1, row + 1):
+    for j in range(1, column + 1):
+        print(i, end = ' ')
+    print()
+
+    
