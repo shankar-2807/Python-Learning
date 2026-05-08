f002928a48af2e77a800a3c9269b16ef0de38637
shankar-2807 <mashalkarshankar9@gmail.com>
2026-05-08 18:58:40 +0530

    Add files via upload

---
+++ lambda_demo.py
@@ -0,0 +1,9 @@
+### Lambda Function
+#1. It is anonymous function
+#2. Basically used to perform operation like expression 
+
+# Syntax
+#lambda parameter : expression
+
+sq = lambda x : x * x
+print(sq(25))
