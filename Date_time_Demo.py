a844bd84f0a2a4b7a65f4d47e4045743e6009aa1
shankar-2807 <mashalkarshankar9@gmail.com>
2026-05-09 11:52:04 +0530

    Add files via upload

---
+++ Date_time_Demo.py
@@ -0,0 +1,15 @@
+import datetime
+
+# print(datetime.datetime.now())
+
+# print(datetime.date.today)
+
+# print(datetime.time(3, 4, 55))
+
+# now = datetime.datetime.now()
+# print(now + datetime.timedelta(days = 7))
+
+# str = '30-4-2025'
+# date = datetime.datetime.strptime(str, "%d-%m-%Y")        "P" means parse: String parsed to date time
+# print(date)  
+
