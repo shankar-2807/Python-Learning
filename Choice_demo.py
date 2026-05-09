b572e675689b91a870dd511ae0cf4ddf513f9b97
shankar-2807 <mashalkarshankar9@gmail.com>
2026-05-09 11:51:42 +0530

    Add files via upload

---
+++ Choice_demo.py
@@ -0,0 +1,10 @@
+import random
+
+dice = [1, 2, 3, 4, 5, 6]
+# print(random.choice(dice))
+
+# print(random.sample(range(1, 100), 10))
+
+random.shuffle(dice)         # change sequence
+print(dice)
+
