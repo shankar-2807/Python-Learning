93c339407d256663b74b8206f61b189848d8ac4a
shankar-2807 <mashalkarshankar9@gmail.com>
2026-05-16 06:06:04 +0530

    Add files via upload

---
+++ ClassA.py
@@ -0,0 +1,10 @@
+class A:
+    def __init__(self):
+        print("This is Constructor of A.")
+
+    def disply(self):
+        print("This is disply method of A.")
+
+    def showData(self):
+        print("This is Show method of A.")
+
