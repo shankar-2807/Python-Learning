0a61201b02bc541b70a3a745b2d2722b29f7bb87
shankar-2807 <mashalkarshankar9@gmail.com>
2026-05-21 18:06:50 +0530

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,11 @@
+#Write a Python program to check if a person is eligible to vote.
+
+num = int(input("Enter a number: "))
+
+if num == 0:
+    print("Number is Zero.")
+elif num < 0:
+    print("Num is Negative.")
+else:
+    print("Num is Positive.") 
+
