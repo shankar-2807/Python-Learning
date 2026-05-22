4d1c5a08af910afb18e0e6131a5a32657eedd3ce
shankar-2807 <mashalkarshankar9@gmail.com>
2026-05-22 18:40:54 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,13 @@
+# '''it should always start with _ & character 
+# they not allowed any specific symbol like (@,#,$,&)  only allowed (_)
+# its start with like ,
+# name = shankar
+# num1 = 18
+# _num2 = 20
+# '''
+
+name = 'shakanr'
+num1 = 18
+_num2 = 20.88
+n1 = 5+8j
+
