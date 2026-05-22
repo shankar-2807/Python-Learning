e948af063f8a353b99163c192da9cc8adbf698d0
shankar-2807 <mashalkarshankar9@gmail.com>
2026-05-22 18:41:38 +0530

    Add files via upload

---
+++ data_Types.py
@@ -0,0 +1,37 @@
+#Numeric data type 
+num1 = 18  #int
+_num2 = 20.88 # folat
+n1 = 5+8j  #complex
+
+
+print(num1)
+print(_num2)
+print(type(num1),type(_num2),type(n1))
+
+
+# None data type
+shankar = None
+print(type(shankar))
+
+#String data type
+name = 'shankar'
+print(type(name))
+
+# List data type
+student_name = ['shankar','pragati','deepali']
+print(type(student_name))
+
+# Tuple data type 
+marks = (10,20,30,40)
+print(type(marks))
+
+
+# Dictionary data type
+classes = {'language':'python'}
+print(type(classes))
+
+# Set data type 
+classessss = {'python'}
+print(type(classessss))
+
+#boolean data type
