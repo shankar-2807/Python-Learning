2caec7c51fd9fb3e8d1a59be1c155fbbe3e491b7
shankar-2807 <mashalkarshankar9@gmail.com>
2026-05-23 20:30:15 +0530

    Add files via upload

--- demo1.py
+++ demo1.py
@@ -1,21 +1,13 @@
-#1. Structure: set(), {10,20,30}
-s = set()
-s = {10, 20, 30}
-print(type(s))
+num1 = 10 
+num2 = 20
+sum = (num1+num2)
+#print(sum)
+
+shankar = (f''' Shankar it is a very good person, topper of the instuite & university , proud of them please everyone clap for them .
+'A very big congratulations to mr.shankar mashalkar
 
 
-#2. Type of data: Heterogenous , unique(value)
-s1 = {10, 'a', 3.14, 10}
-print(s1)
+it's all india ranke is {sum}''')
+print(shankar)
 
-#3. Sequence :  (Unordered)
-s = {10, 20, 'a', 3.14, 30, 'b'}
-print(s)
 
-#4. Changable : (Mutable) , not editable
-# s[0]  # gives TypeError: set notb subscriptable
-
-print(id(s))
-s.add(88)
-print(id(s))
-print(s)
