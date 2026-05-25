815a7c52e37bbbeacabe4f439919e79f49b235ea
shankar-2807 <mashalkarshankar9@gmail.com>
2026-05-25 22:41:28 +0530

    Add files via upload

---
+++ Positional.py
@@ -0,0 +1,18 @@
+def name(first,last):
+    print(first,last)
+
+name('shankar','mashalkar')
+
+
+def sum(a,b):
+    c = a+b
+    print(c)
+
+sum(10,20)
+
+def positional(name,city):
+    print(f'welcome {name} to the {city}')
+
+positional('shankar','pune')
+
+
