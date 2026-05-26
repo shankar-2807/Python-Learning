347e4632381a04f7da7dc3fe5ae131e5fd39946c
shankar-2807 <mashalkarshankar9@gmail.com>
2026-05-26 22:01:19 +0530

    Add files via upload

---
+++ return.py
@@ -0,0 +1,41 @@
+def full_name(name,sirname):
+    full_name= f'{name} {sirname}'
+    return full_name
+
+name = full_name('shankar','mashalkar')
+print(name)
+
+
+# if we use return method then required to create function variable like
+# name = full_name('shankar','mashalkar')
+# print(name)
+
+def names(names,sirnames):
+    names = f"{names} {sirnames}"
+    return names
+
+NameError = names('sagar','swami')
+print(NameError)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# num1 = int(input("Enter num1 : "))
+# num2 = int(input("Enter num2 : "))
+
+# select_choice = input('Enter Choice: +, -, *, / ')
+
+# if select_choice == '+':
+#     print(num1+num2)
+
