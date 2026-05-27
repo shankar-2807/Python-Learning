33007c77850146e5604a71cf8555868169867ce8
shankar-2807 <mashalkarshankar9@gmail.com>
2026-05-27 21:43:58 +0530

    Add files via upload

---
+++ continuebreakpass.py
@@ -0,0 +1,20 @@
+###Continue
+
+for i in range(1,11):
+    if i == 3:
+        continue
+    print(i)
+
+####Break
+for p in range(1,11):
+    if p == 5:
+        break
+    print(p)
+
+
+####Pass
+for s in range(1,11):
+    if s == 8:
+        pass
+    print(s)
+
