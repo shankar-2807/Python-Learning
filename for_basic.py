5a17cf73137a9c48793739cbd73dae9262935bef
shankar-2807 <mashalkarshankar9@gmail.com>
2026-05-28 21:29:58 +0530

    Add files via upload

---
+++ for_basic.py
@@ -0,0 +1,19 @@
+
+
+# for i in range(1,6):
+#     print(i)
+
+# li= [1,2,3]
+# li.append(8)
+# print(li)
+
+
+def my_sum(a=5, b=5):
+    c = a + b
+    return c
+
+# result = my_sum(2, 5)
+# print(result)
+
+
+my_sum()
