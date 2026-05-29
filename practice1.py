028ba0fbf8c0425a06488fa9d05a757475945239
shankar-2807 <mashalkarshankar9@gmail.com>
2026-05-29 17:15:27 +0530

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,86 @@
+# ### Print even numbers
+# for i in range(1,11):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+# print('###################')
+# ### Print odd number
+# for i in range (1,10):
+#     if i % 2 !=0:
+#         continue
+#     print(i)
+
+
+# for i in range(3,31,3):
+#     print(i)
+
+# num = int(input("Enter a number: "))
+# def padha (num):
+#     for i in range(num,num*10+1,num):
+#         print(i)
+
+# padha(num)
+
+# num1 = int(input("Enter no to start: "))
+# num2 = int(input("Enter no to End: "))
+
+# print('select option:')
+# print('1.Even')
+# print('2.Odd')
+
+# choice = int(input("Enter your choice (1 or 2): "))
+
+# if choice == 1:
+#     for i in range(num1,num2+1):
+#         if i % 2 == 0:
+#             print(i)
+# elif choice == 2:
+#     for i in range(num1,num2+1):
+#         if i % 2 != 0:
+#             print(i)
+# else:
+#     print("invalid choice...")
+
+
+
+
+# num1 = int(input("Enter no to start: "))
+# num2 = int(input("Enter no to stop: "))
+
+# print('''
+# select Choice:
+# 1.Even
+# 2.Odd''')
+
+# choice = int(input("Enter Choice: "))
+
+# if choice == 1:
+#     for i in range(num1,num2+1):
+#         if i % 2 == 0:
+#             print(i)
+# elif choice == 2:
+#     for i in range(num1,num2+1):
+#         if i % 2 != 0:
+#             print(i)
+# else:
+#     print("invalid choice...")
+
+
+
+# def local(a,b):
+#     c = a*b
+#     print(c)
+
+# local(3,5)
+
+a = int(input("Enter no to start: "))
+b = int(input("Enter no to start: "))
+
+def local ():
+    c = a * b
+    print(c)
+
+local()
+
+
