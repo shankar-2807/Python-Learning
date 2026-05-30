9cfb6d8e9a3f20ca70dae25ee96614021703570d
shankar-2807 <mashalkarshankar9@gmail.com>
2026-05-30 18:41:03 +0530

    Add files via upload

---
+++ 3string.py
@@ -0,0 +1,19 @@
+## String 
+
+# it is denoted by ' ' , " "
+# Order : it is maintain values in insertion order.
+# immutuable : after creation you can not be change (index is allowed)
+# Duplicate : it can be allowed duplicate values
+
+
+##Syntax  
+
+string = 'shankar', 'mashalkar', 'shankar'
+print(string.index('mashalkar'))
+print(string)
+
+##Not allowed
+
+string.append('sagar')
+string.remove('mashalkar')
+
