6fd3656054e25946f50f6cb15261e8418b0c5f9d
shankar-2807 <mashalkarshankar9@gmail.com>
2026-05-30 18:40:06 +0530

    Add files via upload

---
+++ 1list.py
@@ -0,0 +1,19 @@
+## List 
+
+# it is denoted by []
+# Order : it is maintain values in insertion order.
+# Mutuable : after creation you can change it allowed (append,remove)
+# Duplicate : it can be allowed duplicate values
+
+##Syntax
+
+li = [10,20,30,40,40]
+print(li)
+
+print('list after changes')
+
+li[0]=5
+li.append(70)
+li.remove(20)
+print(li)
+
