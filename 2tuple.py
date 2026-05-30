c8795fbe2b069ee5e110cf4a0d668f6e34f3ba0c
shankar-2807 <mashalkarshankar9@gmail.com>
2026-05-30 18:40:32 +0530

    Add files via upload

---
+++ 2tuple.py
@@ -0,0 +1,19 @@
+## Tuple 
+
+# it is denoted by ()
+# Order : it is maintain values in insertion order.
+# immutuable : after creation you can not be change (index is allowed)
+# Duplicate : it can be allowed duplicate values
+
+##Syntax
+
+tuple = (10,20,30,40,40)
+print(tuple)
+print(tuple.index(20))
+
+#Not allowed
+
+tuple.append(60)
+tuple.remove(20)
+
+
