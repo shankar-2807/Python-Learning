4eaf609a5f7d2e00bc4301ebda510ba9ab0d3c5e
shankar-2807 <mashalkarshankar9@gmail.com>
2026-05-31 17:10:19 +0530

    Add files via upload

---
+++ 5set.py
@@ -0,0 +1,22 @@
+## Set 
+
+# it is denoted by { } 
+# Unorder : it does not maintain values in insertion order.
+# Mutuable : after creation you can be change allowed (add,remove) but (index is not allowed)
+# Duplicate : does not allowed duplicate values
+
+
+
+##Syntax  
+
+set = {10, 20, 30, 40}
+print(set)
+set.add(70)
+set.remove(20)
+print(set)
+
+##Not allowed
+
+print(set.index(30))
+
+
