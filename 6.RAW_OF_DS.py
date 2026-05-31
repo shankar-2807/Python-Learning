42a561b37eb65d534de499bd984d9e2f01436e98
shankar-2807 <mashalkarshankar9@gmail.com>
2026-05-31 17:10:50 +0530

    Add files via upload

---
+++ 6.RAW_OF_DS.py
@@ -0,0 +1,27 @@
+# set = {10, 20, 30}
+# set.add(40)
+# print(set)
+
+# li = [10,20,30,40]
+# li[0]=5
+# li.append(70)
+# li.remove(20)
+# print(li.index(30))
+
+student = {'name': 'shankar','age': 22,'course': 'python'}
+student['name']='abhishek'
+print(student)
+student.update({'age':22, 'course':'data science'})
+print(student)
+
+# tuple=(10,20,30,40)
+# tuple[0]=5
+# print(tuple)
+
+# string = 'shankar','mashalkar'
+# string[0]='abhishek'
+# print(string)
+
+# fs = frozenset([10, 20, 30])
+# fs.add(40)   # Error
+
