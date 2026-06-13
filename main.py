ca9d3e3bf25edc64565f9bbe4420e132bbca6c81
shankar-2807 <mashalkarshankar9@gmail.com>
2026-06-13 08:08:17 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from admin import Admin
 
-app = Flask(__name__)
+def main():
+    ch = 0
+    while(ch != '2'):
+        print('''---EMPLOYEE MANAGEMENT SYSTEM---
+        1. Login
+        2. Exit''')
+        ch = input('Enter choice:')
+        if(ch == '1'):
+            uname = input('Enter username:')
+            passw = input('Enter password:')
+            if(uname == 'admin' and passw == '1234'):
+                print('Logged in successful')
+                Admin()
+            else:
+                print('Invalid credentials....')
+        elif(ch == '2'):
+            print('Thank you!')
+        else:
+            print('Invalid choice...')
 
+if(__name__ == '__main__'):
+    main()
 
-from urls import *
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
 
