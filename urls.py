d9dbf8fad6b408755ee327e892c6f5b42360de92
shankar-2807 <mashalkarshankar9@gmail.com>
2026-06-20 23:45:58 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,15 +1,7 @@
-from main import app
+from django.contrib import admin
+from django.urls import path,include
 
-import app as book
-
-#------------Book-----------#
-app.add_url_rule("/add_book", view_func=book.add_book, methods=["GET", "POST"])
-
-# app.add_url_rule("/edit_book/<bid>",view_func=book.edit_book,methods=["GET","POST"])
-
-# app.add_url_rule("/delete_book/<bid>",view_func=book.delete_book,methods=["GET","POST"])
-
-# app.add_url_rule("/search_book",view_func=book.search_book)
-
-
-
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('MyApp.urls')),
+]
