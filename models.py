101ff6fccf7ab3909de045345df6aa81f2d3834f
shankar-2807 <mashalkarshankar9@gmail.com>
2026-07-03 07:01:22 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from django import models
 
-# Create your models here.
+class Category(models.model):
+    cname = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = "Category"
+
+    def __str__(self):
+        return self.cname
+
+
+class Cake(models.model):
+    cake_name = models.CharField(max_length=20)
+    price = models.CharField(default=200)
+    description = models.Charfield(max_length=100)
+    image_url = models.ImageField(default='abc.jpg', upload_to='static/Images')
+    category = models.Foreginkey(Category,on_delete=models.CASCADE)
+
+    class Meta:
+        db_cake = "Cake"
+
+
+
+
