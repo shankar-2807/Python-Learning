25dc8d7ad620d5ef39ef33e99ca92c7024d51a92
shankar-2807 <mashalkarshankar9@gmail.com>
2026-07-10 19:26:47 +0530

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for CakeshopApp project.
+WSGI config for ModelDemmo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CakeshopApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ModelDemmo.settings')
 
 application = get_wsgi_application()
