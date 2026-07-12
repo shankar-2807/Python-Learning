512a539b33b10ebed0bc78db8c1fafc9481fe7e2
shankar-2807 <mashalkarshankar9@gmail.com>
2026-07-12 16:02:51 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,10 +1,15 @@
-
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('myApp/',include('MyApp.urls'))
-]
+    # path('hello',views.helloFromFirst),
+    # path('demo',views.demoHtml),
+    # path('login', views.login),
+    path('displayName',views.displayName),
+    path('displayList',views.displayList),
+    path('displayDict',views.displayDict),
+    path('displayStudent',views.displayStudent),
 
+    
+]
 
