8a4a7bff833b9b27cf92f786d6e3ca928d23dabc
shankar-2807 <mashalkarshankar9@gmail.com>
2026-07-24 21:42:53 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello',views.helloFromMyApp2),
+    # path('hello',views.helloFromFirst),
+    # path('demo',views.demoHtml),
+    # path('login', views.login),
+    path('displayName',views.displayName),
+    path('displayList',views.displayList),
+    path('displayDict',views.displayDict),
+    path('displayStudent',views.displayStudent),
+
+    
 ]
 
