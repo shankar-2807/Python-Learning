c79127fb24597a50eacf30178f34d57c5c8d1c27
shankar-2807 <mashalkarshankar9@gmail.com>
2026-08-06 23:21:32 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
-from admin import Admin
+from principal import SchoolSystem
+from menus import Menus
 
 def main():
-    ch = 0
-    while(ch != '2'):
-        print('''---EMPLOYEE MANAGEMENT SYSTEM---
-        1. Login
-        2. Exit''')
-        ch = input('Enter choice:')
-        if(ch == '1'):
-            uname = input('Enter username:')
-            passw = input('Enter password:')
-            if(uname == 'admin' and passw == '1234'):
-                print('Logged in successful')
-                Admin()
-            else:
-                print('Invalid credentials....')
-        elif(ch == '2'):
-            print('Thank you!')
-        else:
-            print('Invalid choice...')
+    school = SchoolSystem()
+    menus = Menus(school)
 
-if(__name__ == '__main__'):
+    while True:
+        print("""\nWelcome to College Management System
+                1. Principal Login
+                2. Student Login
+                3. Exit """)
+        choice = input("Enter choice: ").strip()
+        if choice == "1":
+            if school.principal_login():
+                menus.principal_menu()
+        elif choice == "2":
+            student = school.student_login()
+            if student:
+                menus.student_menu(student)
+        elif choice == "3":
+            print("Exiting... Goodbye!")
+            break
+        else:
+            print("Invalid choice!")
+
+
+if __name__ == "__main__":
     main()
 
-
+    
