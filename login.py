1d0a89186e4e2a50909be2ae630faf6193418c38
shankar-2807 <mashalkarshankar9@gmail.com>
2026-08-12 23:15:38 +0530

    Add files via upload

--- login.py
+++ login.py
@@ -1,31 +1,24 @@
 import getpass
-from helpers import Helper
+from helpers import prompt_nonempty, find_by_key
 from principal import principal_password, students
 from menus import principal_menu, student_menu
 
+def principal_login():
+    pwd = getpass.getpass("Enter Principal Password: ")
+    if pwd == principal_password:
+        principal_menu()
+    else:
+        print("Incorrect password!")
 
-class LoginSystem:
-    def __init__(self):
-        # imported values remain as references
-        self.principal_password = principal_password
-        self.students = students
-
-    def principal_login(self):
-        pwd = getpass.getpass("Enter Principal Password: ")
-        if pwd == self.principal_password:
-            principal_menu()
-        else:
-            print("Incorrect password!")
-
-    def student_login(self):
-        roll = Helper.prompt_nonempty("Enter Roll No: ")
-        st = Helper.find_by_key(self.students, "roll", roll)
-        if not st:
-            print("Student not found! Ask Principal to add your account.")
-            return
-        pwd = getpass.getpass("Enter Password: ")
-        if pwd != st.get("password"):
-            print("Incorrect password!")
-            return
-        student_menu(st)
+def student_login():
+    roll = prompt_nonempty("Enter Roll No: ")
+    st = find_by_key(students, "roll", roll)
+    if not st:
+        print("Student not found! Ask Principal to add your account.")
+        return
+    pwd = getpass.getpass("Enter Password: ")
+    if pwd != st.get("password"):
+        print("Incorrect password!")
+        return
+    student_menu(st)
 
