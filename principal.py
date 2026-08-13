5987fd80201b2b6e4d016e0e0ce62b196e5f47cb
shankar-2807 <mashalkarshankar9@gmail.com>
2026-08-13 22:34:59 +0530

    Add files via upload

--- principal.py
+++ principal.py
@@ -1,186 +1,188 @@
 import getpass
-from helpers import Helper
+from helpers import prompt_nonempty, ensure_unique, find_by_key, search_keyword, show_all
 
-class SchoolSystem:
-    def __init__(self):
-        self.courses = []
-        self.staff = []
-        self.students = []
-        self.notices = []
-        self.principal_password = "admin123"
 
-    # ---------- LOGIN ----------
-    def principal_login(self):
-        pwd = getpass.getpass("Enter Principal Password: ")
-        if pwd == self.principal_password:
-            print("Login successful! Welcome Principal.")
-            return True
-        else:
-            print("Incorrect password!")
-            return False
+courses = []
+staff = []
+students = []
+notices = []
 
-    def student_login(self):
-        roll = Helper.prompt_nonempty("Enter Student Roll No: ")
-        st = Helper.find_by_key(self.students, "roll", roll)
-        if not st:
-            print("Student not found!")
-            return None
-        pwd = getpass.getpass("Enter Password: ")
-        if pwd == st["password"]:
-            print(f"Welcome {st['name']}!")
-            return st
-        else:
-            print("Incorrect password!")
-            return None
+principal_password = "admin123"
 
-    # ---------- Principal password ----------
-    def principal_change_password(self):
-        old = getpass.getpass("Enter old password: ")
-        if old != self.principal_password:
-            print("Incorrect old password!")
-            return
-        new = getpass.getpass("Enter new password: ")
-        if not new:
-            print("New password cannot be empty.")
-            return
-        self.principal_password = new
-        print("Password changed successfully!")
+# ---------- LOGIN FUNCTIONS ----------
+def principal_login():
+    global principal_password
+    pwd = getpass.getpass("Enter Principal Password: ")
+    if pwd == principal_password:
+        print(" Login successful! Welcome Principal.")
+        return True
+    else:
+        print("Incorrect password!")
+        return False
 
-    # ---------- Course CRUD ----------
-    def add_course(self):
-        cid = Helper.prompt_nonempty("Enter Course ID: ")
-        if not Helper.ensure_unique(self.courses, "id", cid):
-            print("Course ID already exists!")
-            return
-        name = Helper.prompt_nonempty("Enter Course Name: ")
-        self.courses.append({"id": cid, "name": name})
-        print("Course added successfully!")
+def student_login():
+    roll = prompt_nonempty("Enter Student Roll No: ")
+    st = find_by_key(students, "roll", roll)
+    if not st:
+        print(" Student not found!")
+        return None
+    pwd = getpass.getpass("Enter Password: ")
+    if pwd == st["password"]:
+        print(f"Welcome {st['name']}!")
+        return st   
+    else:
+        print(" Incorrect password!")
+        return None
 
-    def update_course(self):
-        cid = Helper.prompt_nonempty("Enter Course ID to update: ")
-        c = Helper.find_by_key(self.courses, "id", cid)
-        if not c:
-            print("Course not found!")
-            return
-        c["name"] = Helper.prompt_nonempty("Enter New Course Name: ")
-        print("Course updated!")
+# ---------- Principal password change ----------
+def principal_change_password():
+    global principal_password
+    old = getpass.getpass("Enter old password: ")
+    if old != principal_password:
+        print("Incorrect old password!")
+        return
+    new = getpass.getpass("Enter new password: ")
+    if not new:
+        print("New password cannot be empty.")
+        return
+    principal_password = new
+    print("Password changed successfully!")
 
-    def delete_course(self):
-        cid = Helper.prompt_nonempty("Enter Course ID to delete: ")
-        c = Helper.find_by_key(self.courses, "id", cid)
-        if not c:
-            print("Course not found!")
-            return
-        self.courses.remove(c)
-        print("Course deleted!")
+# ---------- Course CRUD ----------
+def add_course():
+    cid = prompt_nonempty("Enter Course ID: ")
+    if not ensure_unique(courses, "id", cid):
+        print("Course ID already exists!")
+        return
+    name = prompt_nonempty("Enter Course Name: ")
+    courses.append({"id": cid, "name": name})
+    print("Course added successfully!")
 
-    def search_course(self):
-        kw = Helper.prompt_nonempty("Enter keyword (id/name): ")
-        res = Helper.search_keyword(self.courses, ["id", "name"], kw)
-        Helper.show_all(res, "Course Search Results")
+def update_course():
+    cid = prompt_nonempty("Enter Course ID to update: ")
+    c = find_by_key(courses, "id", cid)
+    if not c:
+        print("Course not found!")
+        return
+    c["name"] = prompt_nonempty("Enter New Course Name: ")
+    print("Course updated!")
 
-    # ---------- Staff CRUD ----------
-    def add_staff(self):
-        sid = Helper.prompt_nonempty("Enter Staff ID: ")
-        if not Helper.ensure_unique(self.staff, "id", sid):
-            print("Staff ID already exists!")
-            return
-        name = Helper.prompt_nonempty("Enter Staff Name: ")
-        dept = Helper.prompt_nonempty("Enter Department: ")
-        self.staff.append({"id": sid, "name": name, "dept": dept})
-        print("Staff added successfully!")
+def delete_course():
+    cid = prompt_nonempty("Enter Course ID to delete: ")
+    c = find_by_key(courses, "id", cid)
+    if not c:
+        print("Course not found!")
+        return
+    courses.remove(c)
+    print("Course deleted!")
 
-    def update_staff(self):
-        sid = Helper.prompt_nonempty("Enter Staff ID to update: ")
-        s = Helper.find_by_key(self.staff, "id", sid)
-        if not s:
-            print("Staff not found!")
-            return
-        s["name"] = Helper.prompt_nonempty("Enter New Staff Name: ")
-        s["dept"] = Helper.prompt_nonempty("Enter New Department: ")
-        print("Staff updated!")
+def search_course():
+    kw = prompt_nonempty("Enter keyword (id/name): ")
+    res = search_keyword(courses, ["id", "name"], kw)
+    show_all(res, "Course Search Results")
 
-    def delete_staff(self):
-        sid = Helper.prompt_nonempty("Enter Staff ID to delete: ")
-        s = Helper.find_by_key(self.staff, "id", sid)
-        if not s:
-            print("Staff not found!")
-            return
-        self.staff.remove(s)
-        print("Staff deleted!")
+# ---------- Staff CRUD ----------
+def add_staff():
+    sid = prompt_nonempty("Enter Staff ID: ")
+    if not ensure_unique(staff, "id", sid):
+        print("Staff ID already exists!")
+        return
+    name = prompt_nonempty("Enter Staff Name: ")
+    dept = prompt_nonempty("Enter Department: ")
+    staff.append({"id": sid, "name": name, "dept": dept})
+    print("Staff added successfully!")
 
-    def search_staff(self):
-        kw = Helper.prompt_nonempty("Enter keyword (id/name/dept): ")
-        res = Helper.search_keyword(self.staff, ["id", "name", "dept"], kw)
-        Helper.show_all(res, "Staff Search Results")
+def update_staff():
+    sid = prompt_nonempty("Enter Staff ID to update: ")
+    s = find_by_key(staff, "id", sid)
+    if not s:
+        print("Staff not found!")
+        return
+    s["name"] = prompt_nonempty("Enter New Staff Name: ")
+    s["dept"] = prompt_nonempty("Enter New Department: ")
+    print("Staff updated!")
 
-    # ---------- Student CRUD ----------
-    def add_student(self):
-        roll = Helper.prompt_nonempty("Enter Student Roll No: ")
-        if not Helper.ensure_unique(self.students, "roll", roll):
-            print("Student roll already exists!")
-            return
-        name = Helper.prompt_nonempty("Enter Student Name: ")
-        pwd = getpass.getpass("Set Student Password: ") or "1234"
-        self.students.append({"roll": roll, "name": name, "password": pwd})
-        print("Student added successfully! (default password set)")
+def delete_staff():
+    sid = prompt_nonempty("Enter Staff ID to delete: ")
+    s = find_by_key(staff, "id", sid)
+    if not s:
+        print("Staff not found!")
+        return
+    staff.remove(s)
+    print("Staff deleted!")
 
-    def update_student(self):
-        roll = Helper.prompt_nonempty("Enter Student Roll No to update: ")
-        st = Helper.find_by_key(self.students, "roll", roll)
-        if not st:
-            print("Student not found!")
-            return
-        st["name"] = Helper.prompt_nonempty("Enter New Student Name: ")
-        print("Student updated!")
+def search_staff():
+    kw = prompt_nonempty("Enter keyword (id/name/dept): ")
+    res = search_keyword(staff, ["id", "name", "dept"], kw)
+    show_all(res, "Staff Search Results")
 
-    def delete_student(self):
-        roll = Helper.prompt_nonempty("Enter Student Roll No to delete: ")
-        st = Helper.find_by_key(self.students, "roll", roll)
-        if not st:
-            print("Student not found!")
-            return
-        self.students.remove(st)
-        print("Student deleted!")
+# ---------- Student CRUD ----------
+def add_student():
+    roll = prompt_nonempty("Enter Student Roll No: ")
+    if not ensure_unique(students, "roll", roll):
+        print("Student roll already exists!")
+        return
+    name = prompt_nonempty("Enter Student Name: ")
+    pwd = getpass.getpass("Set Student Password: ") or "1234"
+    students.append({"roll": roll, "name": name, "password": pwd})
+    print("Student added successfully! (default password set)")
 
-    def search_student(self):
-        kw = Helper.prompt_nonempty("Enter keyword (roll/name): ")
-        res = Helper.search_keyword(self.students, ["roll", "name"], kw)
-        Helper.show_all(res, "Student Search Results")
+def update_student():
+    roll = prompt_nonempty("Enter Student Roll No to update: ")
+    st = find_by_key(students, "roll", roll)
+    if not st:
+        print("Student not found!")
+        return
+    st["name"] = prompt_nonempty("Enter New Student Name: ")
+    print("Student updated!")
 
-    # ---------- Notice CRUD ----------
-    def add_notice(self):
-        nid = Helper.prompt_nonempty("Enter Notice ID: ")
-        if not Helper.ensure_unique(self.notices, "id", nid):
-            print("Notice ID already exists!")
-            return
-        title = Helper.prompt_nonempty("Enter Notice Title: ")
-        msg = Helper.prompt_nonempty("Enter Notice Message: ")
-        self.notices.append({"id": nid, "title": title, "msg": msg})
-        print("Notice added successfully!")
+def delete_student():
+    roll = prompt_nonempty("Enter Student Roll No to delete: ")
+    st = find_by_key(students, "roll", roll)
+    if not st:
+        print("Student not found!")
+        return
+    students.remove(st)
+    print("Student deleted!")
 
-    def update_notice(self):
-        nid = Helper.prompt_nonempty("Enter Notice ID to update: ")
-        n = Helper.find_by_key(self.notices, "id", nid)
-        if not n:
-            print("Notice not found!")
-            return
-        n["title"] = Helper.prompt_nonempty("Enter New Title: ")
-        n["msg"] = Helper.prompt_nonempty("Enter New Message: ")
-        print("Notice updated!")
+def search_student():
+    kw = prompt_nonempty("Enter keyword (roll/name): ")
+    res = search_keyword(students, ["roll", "name"], kw)
+    show_all(res, "Student Search Results")
 
-    def delete_notice(self):
-        nid = Helper.prompt_nonempty("Enter Notice ID to delete: ")
-        n = Helper.find_by_key(self.notices, "id", nid)
-        if not n:
-            print("Notice not found!")
-            return
-        self.notices.remove(n)
-        print("Notice deleted!")
+# ---------- Notice CRUD ----------
+def add_notice():
+    nid = prompt_nonempty("Enter Notice ID: ")
+    if not ensure_unique(notices, "id", nid):
+        print("Notice ID already exists!")
+        return
+    title = prompt_nonempty("Enter Notice Title: ")
+    msg = prompt_nonempty("Enter Notice Message: ")
+    notices.append({"id": nid, "title": title, "msg": msg})
+    print("Notice added successfully!")
 
-    def search_notice(self):
-        kw = Helper.prompt_nonempty("Enter keyword (id/title/msg): ")
-        res = Helper.search_keyword(self.notices, ["id", "title", "msg"], kw)
-        Helper.show_all(res, "Notice Search Results")
+def update_notice():
+    nid = prompt_nonempty("Enter Notice ID to update: ")
+    n = find_by_key(notices, "id", nid)
+    if not n:
+        print("Notice not found!")
+        return
+    n["title"] = prompt_nonempty("Enter New Title: ")
+    n["msg"] = prompt_nonempty("Enter New Message: ")
+    print("Notice updated!")
 
-        
+def delete_notice():
+    nid = prompt_nonempty("Enter Notice ID to delete: ")
+    n = find_by_key(notices, "id", nid)
+    if not n:
+        print("Notice not found!")
+        return
+    notices.remove(n)
+    print("Notice deleted!")
+
+def search_notice():
+    kw = prompt_nonempty("Enter keyword (id/title/msg): ")
+    res = search_keyword(notices, ["id", "title", "msg"], kw)
+    show_all(res, "Notice Search Results")
+
+
