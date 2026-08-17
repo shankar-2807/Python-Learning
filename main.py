df8be3919d6dba592ab37b876c91d9441c213fe9
shankar-2807 <mashalkarshankar9@gmail.com>
2026-08-17 22:50:59 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
-import getpass
-from helpers import prompt_nonempty, find_by_key
-from principal import principal_password, students
-from menus import principal_menu, student_menu
-from principal import principal_login, student_login  
+from login import Login
 
 def main():
-    while True:
-        print("""\nWelcome to College Management System
-                1. Principal Login
-                2. Student Login
-                3. Exit """)
-        choice = input("Enter choice: ").strip()
-        if choice == "1":
-            if principal_login():
-                principal_menu()
-        elif choice == "2":
-            student = student_login()
-            if student:
-                student_menu(student)  
-        elif choice == "3":
-            print("Exiting... Goodbye!")
-            break
-        else:
-            print("Invalid choice!")
-
+    print("=== College Management System ===")
+    login = Login()
+    login.start()
 
 if __name__ == "__main__":
     main()
