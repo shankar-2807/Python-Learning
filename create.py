1bc7e571fdeb1a0bc81c27e9a676e661e7250705
shankar-2807 <mashalkarshankar9@gmail.com>
2026-08-27 22:52:47 +0530

    Add files via upload

--- create.py
+++ create.py
@@ -1,13 +1,6 @@
-# def greet(a=20,b=40):
-#     c = a+b
-#     print(c)
-
-# greet(10,20)
-
-
-def shankar(a,b):
-    c = a+b
+def create(a,b):
+    c = a + b
     print(c)
 
-shankar(5,7)
+create(5,7)
 
