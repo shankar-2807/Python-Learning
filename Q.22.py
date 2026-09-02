5cdbdc783d2f2baa165a40bc2b219063bca4b998
shankar-2807 <mashalkarshankar9@gmail.com>
2026-09-02 23:10:48 +0530

    Add files via upload

---
+++ Q.22.py
@@ -0,0 +1,29 @@
+# Q.22. What is object & class
+
+### Class
+'''
+What is a Class?
+
+A class is a blueprint or template used to create objects.
+which object that is not exists in real world
+
+✔ Example:'''
+
+class Student:
+    def info(self):
+        print("This is a student")
+
+
+### Object
+'''
+What is an Object?
+
+An object is an instance of a class.
+It is a real-world entity created from a class.
+
+✔ Example:'''
+
+s1 = Student()   # object
+s1.info()        # calling method
+
+
