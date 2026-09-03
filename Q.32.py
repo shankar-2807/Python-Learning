58cf540d8ce4105e24d4e223bcf17cb6682af4e9
shankar-2807 <mashalkarshankar9@gmail.com>
2026-09-03 22:46:14 +0530

    Add files via upload

---
+++ Q.32.py
@@ -0,0 +1,19 @@
+num1 = int(input('Enter num1: '))
+print(f'select option: +, - , * , / ')
+
+num2 = int(input('Enter num2: '))
+
+
+if ('+'):
+    print({num1+num2})
+elif ('-'):
+    print({num1-num2})
+elif ('*'):
+    print({num1*num2})
+elif ('/'):
+    print({num1/num2})
+else:
+    "choose correct option"
+
+
+
