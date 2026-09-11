cd4e226fccde1a45f9843f2262bf4cbdf03e69fc
shankar-2807 <mashalkarshankar9@gmail.com>
2026-09-11 22:36:25 +0530

    Add files via upload

--- Q.1.py
+++ Q.1.py
@@ -1,3 +1,11 @@
-year = int(input("Enter a year: "))
-if year / 4 = 0 :
+#1. What is Variable and explain rules to define variable..
+
+## Variable it is name of values , store the values 
+##  it's not start with number   (1num)
+## it's not allow any special symbol either (_ undescore)
+## it's start with character or underscore (num1, _num1)
+
+num1 = 10
+_num = 20
+
 
