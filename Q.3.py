dc28a2099d6b813753fb57cbebe37b2fbc9efaa5
shankar-2807 <mashalkarshankar9@gmail.com>
2026-09-12 22:53:47 +0530

    Add files via upload

--- Q.3.py
+++ Q.3.py
@@ -1,1 +1,49 @@
+# Q.3 Explain operators in python
+
+# Operators are special symbols used to perform operations on variables and values.
+# there are several types of operators in python like (arithmatic,comparision,logical,assignment,identity,membership)
+
+
+### arithmatic
+'''
+addition (+)
+substraction(-)
+multiplication(*)
+division(-)
+exponential(**)'''
+
+### Comparision Operator
+'''
+equal to (==)
+not equal to (!=)
+less than(<)
+greater than(>)
+(<=)
+(>=)'''
+
+### Logical Operators
+'''
+AND = all condition must be true..
+or = atleast one condition must true..
+not = single operand, reverse
+'''
+
+### Assignment operators
+'''
+is equal (=)
+plus equal (+=)
+minus equal (-=)
+multiplication equal (*=)'''
+
+
+### Identity Operator
+'''
+is 
+is not'''
+
+### Membership operator
+'''
+in
+not in '''
+
 
